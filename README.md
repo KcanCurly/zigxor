de2f207cc5efa0f0560aa07e13e31534eb594457
kcancurly <karadumankcan@gmail.com>
2025-07-27 09:11:47 +0300

    u

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # How to use
 
+## Exe
+
+```zig
+zigxor.exe <input_file> <key> <output_file>
+```
+
+## Library
+
 Add following to build.zig.zon (Hash will change, in order to get the correct hash remove the hash section and build the project, you should get the correct hash)
 
 ```zig
 .dependencies = .{
-    .ZigXor = .{ .url = "https://github.com/KcanCurly/zigxor/archive/refs/tags/v0.0.1-alpha.zip", .hash = "zigxor-A.B.C-HASH" },
+    .ZigXor = .{ .url = "https://github.com/KcanCurly/zigxor/archive/refs/tags/vX.X.X.zip", .hash = "zigxor-A.B.C-HASH" },
     }
 ```
 
@@ -20,9 +28,23 @@ Add following to main.zig
 
 ```zig
 const x = @import("zigxor");
+```
+
+### Example
+
+```zig
+const x = @import("zigxor");
+
+const allocator = std.heap.page_allocator;
+
+const input = "test";
 const input_ptr: [*]u8 = @ptrCast(@constCast(input.ptr));
+
+const output = try allocator.alloc(u8, input.len);
 const output_ptr: [*]u8 = @ptrCast(@constCast(output.ptr));
+
 const key = "WXYZ";
+
 try xorInMemory(input_ptr, input.len, key, output_ptr);
 ```
 
--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 
 /// XORs input with a key. Same function can encrypt or decrypt.
 pub fn xor(input: []u8, key: []const u8) void {
@@ -12,28 +13,26 @@ pub fn xorInMemory(input: [*]u8, input_size: usize, key: []const u8, output: [*]
     const input_ptr: [*]const u8 = @ptrCast(input);
     const input_slice = input_ptr[0..input_size];
 
-    const allocator = std.heap.page_allocator;
-    const decrypted = try allocator.alloc(u8, input_size);
-    defer allocator.free(decrypted);
-
-    // Copy and XOR
-    std.mem.copyForwards(u8, decrypted, input_slice);
-    xor(decrypted, key);
-
-    // Ensure output is valid and writeable
     const output_ptr: [*]u8 = @ptrCast(output);
     const output_slice = output_ptr[0..input_size];
 
-    std.mem.copyForwards(u8, output_slice, decrypted);
+    std.mem.copyForwards(u8, output_slice, input_slice);
+
+    xor(output_slice, key);
 }
 
 test "basic" {
     const allocator = std.heap.page_allocator;
+
     const input = "test";
     const input_ptr: [*]u8 = @ptrCast(@constCast(input.ptr));
+
     const output = try allocator.alloc(u8, input.len);
     const output_ptr: [*]u8 = @ptrCast(@constCast(output.ptr));
+
     const key = "WXYZ";
 
     try xorInMemory(input_ptr, input.len, key, output_ptr);
+
+    try testing.expect(output_ptr[0] == 35);
 }
